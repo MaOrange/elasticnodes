be1b88a776993b173db4687300b239839bf9f608
Chengzhi Ma <744186903@qq.com>
2018-10-13 21:41:09 +0800

    add class inherited from QGraphicsTextItem

---
+++ DoubleClickedGraphicsTextItem.cpp
@@ -0,0 +1,6 @@
+#include "DoubleClickedGraphicsTextItem.h"
+
+DoubleClickedGraphicsTextItem::DoubleClickedGraphicsTextItem()
+{
+
+}
---
+++ DoubleClickedGraphicsTextItem.h
@@ -0,0 +1,13 @@
+#ifndef DOUBLECLICKEDGRAPHICSTEXTITEM_H
+#define DOUBLECLICKEDGRAPHICSTEXTITEM_H
+
+#include <QWidget>
+#include <QGraphicsTextItem>
+
+class DoubleClickedGraphicsTextItem : public QGraphicsTextItem
+{
+public:
+    DoubleClickedGraphicsTextItem();
+};
+
+#endif // DOUBLECLICKEDGRAPHICSTEXTITEM_H
